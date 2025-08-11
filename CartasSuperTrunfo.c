7bace1ece9befad419e2bc770683ca08bc4bd11e
Hescy <143231298+henriqscy@users.noreply.github.com>
2025-08-11 18:17:02 +0000

    Versao 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
-    
-char estado; // Variavel da pergunta do Estado
-char codigo[60]; // Variavel do Codigo de 01 a 04
+
+char estado[30]; // Variavel da pergunta do Estado
+char codigo[40]; // Variavel do Codigo de 01 a 04
 char cidade[50]; // Variavel da Cidade
 int populacao, ponto; // Varivel da quantidade da populacao e ponto turistico
 float area, PIB; // Variavel do valor da Area e do PIB
+
 
 //Usaremos Int para numeros inteiros, Char para caracteres, e float para numeros decimais
 
@@ -13,7 +14,7 @@ float area, PIB; // Variavel do valor da Area e do PIB
 
 printf("Carta 1:\n");
 printf("Escolha uma Letra de 'A' a 'H' (representando um dos oitos estados): ");
-scanf("%c", &estado);
+scanf("%s", &estado);
 
 printf("Escolha o Codigo da Carta (a letra do estado seguida de um numero de 01 a 04): ");
 scanf("%s", &codigo);
@@ -39,9 +40,9 @@ printf("Vamos fazer a carta n. 2 agora!\n");
 
 
 //Carta 2: Aqui criamos a segunda variavel
-char estado2;
+char estado2[50];
 char codigo2[60];
-char cidade2[50];
+char cidade2[70];
 int populacao2, ponto2;
 float area2, pib2;
 
@@ -74,7 +75,7 @@ printf("\n");
 
 printf("Resultado da Carta 1\n");
 printf("Carta 1:\n");
-printf("Estado: %c\n", estado);
+printf("Estado: %s\n", estado);
 printf("Codigo: %s\n", codigo);
 printf("Nome da Cidade: %s\n", cidade);
 printf("Populacao: %d\n", populacao);
@@ -88,7 +89,7 @@ printf("\n");
 
 printf("Resultado da Carta 2\n");
 printf("Carta 2:\n");
-printf("Estado: %c\n", estado2);
+printf("Estado: %s\n", estado2);
 printf("Codigo: %s\n", codigo2);
 printf("Nome da Cidade: %s\n", cidade2);
 printf("Populacao: %d\n", populacao2);
@@ -97,6 +98,4 @@ printf("PIB: %f\n", pib2);
 printf("Numero de Pontos Turisticos: %d\n", ponto2);
 
 return 0;
-}
-    return 0;
 }
